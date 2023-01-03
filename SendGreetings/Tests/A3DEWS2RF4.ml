69fff5203e46df25f2eb16733460b7bd9da85986
sonalisood <70566043+sonalisood@users.noreply.github.com>
2023-01-03 14:18:11 +0530

    Add files via upload

---
+++ SendGreetings/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/SendGreetings/Tests/SendGreetings1.bwt#/"/>
+  <EmulationDataFile>ACED000574001253656E644772656574696E6773312E627774</EmulationDataFile>
+</emulation:BWTFile>
